f17e5a0dc9646c97da8892cf11bdfe52cbea3339
Ditlev Stjerne <ditlev.stjerne@gmail.com>
2018-10-17 17:40:49 +0200

    Added bash script

---
+++ b-cli.sh
@@ -0,0 +1,25 @@
+#/bin/bash
+
+send_to_address() {
+    echo "Enter address"
+    read address
+    echo "Enter amount to send"
+    read amount
+    echo $(bitcoin-cli -regtest sendtoaddress $address $amount)
+}
+
+while getopts ":bsnl" opt; do
+    case $opt in
+        b)
+            echo $(bitcoin-cli -regtest getbalance);;
+        s)
+            send_to_address;;
+        n)
+            echo $(bitcoin-cli -regtest getnewaddress);;
+        l)
+            echo $(bitcoin-cli -regtest listunspent);;
+        \?)
+            echo "invalid option";;
+        exit 1;;
+    esac
+done
